757ec27c32d91e27b650e65af2beea87e79c09c5
sathish15meb34 <davidsathish34@gmail.com>
2022-03-15 12:38:07 +0530

    Project Structure

---
+++ src/test/java/org/test/A.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class A {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("test1");
+		System.out.println("test 2");
+		System.out.println("test3");
+	}
+
+	 }
